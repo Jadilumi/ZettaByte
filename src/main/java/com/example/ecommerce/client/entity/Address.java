d056f4c34b836f1df0ed37399cf7de4b3ad5dd8c
Dielder <dieldergabrieleal@hotmail.com>
2024-09-29 15:48:19 -0300

    creating a relationship between the customer and address tables

---
+++ src/main/java/com/example/ecommerce/client/entity/Address.java
@@ -0,0 +1,42 @@
+package com.example.ecommerce.client.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+@Table(name = "addresses")
+public class Address {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "address_id")
+	private Long addressId;
+
+	@ManyToOne
+	@JoinColumn(name = "client_id", nullable = false)
+	private Client client;
+
+	@Column(nullable = false)
+	private String street;
+
+	@Column(nullable = false)
+	private String city;
+
+	@Column(nullable = false)
+	private String state;
+
+	@Column(nullable = false)
+	private String number;
+
+	@Column(name = "postal_code", nullable = false)
+	private String postalCode;
+}
